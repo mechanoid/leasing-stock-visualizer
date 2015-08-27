b622bbe6ce23d9b7fe8963637e11de2f4a78bbf7
Falk Hoppe <falkhoppe81@gmail.com>
2015-08-27 19:59:39 +0200

    added test for broadcasting new items on success

---
+++ client/app/components/stock-item-form/stock-item-form-test.js
@@ -0,0 +1,40 @@
+/*jslint browser: true, devel: true, todo: true, white: true, indent: 2, nomen: true, regexp: true */
+/*global  module, inject, it, beforeEach, expect, describe, spyOn */
+
+describe('StockItemFormController', function(){
+  "use strict";
+
+  var addItemController, scope, items, deferred;
+
+  beforeEach(module('leasingStockApp'));
+
+  beforeEach(inject(function ($controller, $q, $rootScope) {
+    items = {
+      addItem: function () {
+        deferred = $q.defer();
+        return deferred.promise;
+      }
+    };
+
+    spyOn(items, 'addItem').and.callThrough();
+    spyOn($rootScope, '$broadcast').and.callThrough();
+
+    scope = $rootScope.$new();
+
+    addItemController = function () {
+      return $controller('StockItemFormCtrl', {
+          $scope: scope,
+          items: items
+      });
+    };
+  }));
+
+  it('should propagate item updated via global event', inject(function($rootScope) {
+    addItemController();
+    scope.addItem("item_1");
+    deferred.resolve();
+    scope.$digest();
+    expect($rootScope.$broadcast).toHaveBeenCalledWith("item.created", "item_1");
+  }));
+
+});
